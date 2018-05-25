bf625049f8038f8fd7483838dfd2f34c69a76aff
sudz123 <sudarshan.kamath97@gmail.com>
2018-05-25 10:34:29 +0900

    Changed arduino code JSN270

--- JSN270_TCP_Client_WPA2/JSN270_TCP_Client_WPA2.ino
+++ JSN270_TCP_Client_WPA2/JSN270_TCP_Client_WPA2.ino
@@ -2,6 +2,7 @@
 #include <JSN270.h>
 #include <Arduino.h>
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 #define SSID      "stratoit2"		// your wifi network SSID
 #define KEY       "strato1010"		// your wifi network password
@@ -10,8 +11,9 @@ int out1 = 9;
 int out2 = 10;
 String val1;
 int val;
+char c;
 
-#define USE_DHCP_IP 1
+#define USE_DHCP_IP 15
 
 #if !USE_DHCP_IP
 #define MY_IP          "192.168.1.133"
@@ -27,15 +29,22 @@ SoftwareSerial mySerial(3, 2); // RX, TX
  
 JSN270 JSN270(&mySerial);
 
+Servo Throttle, Steer;
+
+void setSpeed(int speed)
+  {
+    int angle = map(speed, 0, 1000, 0, 180); //Sets servo positions to different speeds
+    Throttle.write(angle);
+  }
+
+
 void setup() {
 	char c;
 
 	mySerial.begin(9600);
 	Serial.begin(9600);
-        pinMode(9,OUTPUT);
-        pinMode(9,OUTPUT);
-        pinMode(10,OUTPUT);
-        pinMode(10,OUTPUT);
+        Throttle.attach(10); //Adds ESC to certain pin. arm();
+        Steer.attach(9); 
 
 	Serial.println("--------- JSN270 TCP Client with WPA2 Test --------");
 
@@ -98,40 +107,23 @@ void setup() {
 }
 
 void loop() {
-	if(JSN270.available()) {
-                val1="";
-                val=JSN270.read()-48;
-                Serial.print(val);
-                //Serial.print(val);
-                //JSN270.print((char)val);
-                if (val == 0) {analogWrite(out1, 0)  ; Serial.println("Speed is = 0"); val = 100;}
-                if (val == 1) {analogWrite(out1, 255); delay(10); analogWrite(out1, 175) ; Serial.println("Speed is = 1"); val = 100;}
-                if (val == 2) {analogWrite(out1, 255); delay(10); analogWrite(out1, 185) ; Serial.println("Speed is = 2"); val = 100;}
-                if (val == 3) {analogWrite(out1, 255); delay(10); analogWrite(out1, 195); Serial.println("Speed is = 3"); val = 100;}
-                if (val == 4) {analogWrite(out1, 255); delay(10); analogWrite(out1, 205); Serial.println("Speed is = 4"); val = 100;}
-                if (val == 5) {analogWrite(out1, 255); delay(10); analogWrite(out1, 215); Serial.println("Speed is = 5"); val = 100;}
-                if (val == 6) {analogWrite(out1, 255); delay(10); analogWrite(out1, 225); Serial.println("Speed is = 6"); val = 100;}
-                if (val == 7) {analogWrite(out1, 255); delay(10); analogWrite(out1, 235); Serial.println("Speed is = 7"); val = 100;}
+        val1="";
+	while(JSN270.available()) {
+                c=JSN270.read();
+                val1+=c;  
+                if(!JSN270.available()){
+                  val = atoi(val1.c_str());
+                  Serial.print(val);
+                  Serial.println("");
+                  if(val > 2    00) {
+                    setSpeed(val); //Creates variable for speed to be used in in for loop  
+                    // runs at 539
+                  }
+                  if(val > 10 && val < 190){
+                    Steer.write(val);
+                  }
+                }
                 
-               // Right Angle Control
-                if (val == 10) {analogWrite(out2, 170)  ; Serial.println(" Right Angle is = 0"); val = 100;}
-                if (val == 11) {analogWrite(out2, 255); delay(10); analogWrite(out2, 180) ; Serial.println(" Right Angle is = 1"); val = 100;}
-                if (val == 12) {analogWrite(out2, 255); delay(10); analogWrite(out2, 190) ; Serial.println(" Right Angle is = 2"); val = 100;}
-                if (val == 13) {analogWrite(out2, 255); delay(10); analogWrite(out2, 200); Serial.println(" Right Angle is = 3"); val = 100;} 
-                if (val == 14) {analogWrite(out2, 255); delay(10); analogWrite(out2, 210); Serial.println(" Right Angle is = 4"); val = 100;}
-                if (val == 15) {analogWrite(out2, 255); delay(10); analogWrite(out2, 220); Serial.println(" Right Angle is = 5"); val = 100;}
-                if (val == 16) {analogWrite(out2, 255); delay(10); analogWrite(out2, 230); Serial.println(" Right Angle is = 6"); val = 100;}
-                if (val == 17) {analogWrite(out2, 255); delay(10); analogWrite(out2, 240); Serial.println(" Right Angle is = 7"); val = 100;}
-            
-               // Left Angle Control
-                if (val == 20) {analogWrite(out2, 170)  ; Serial.println(" Left Angle is = 0"); val = 100;}
-                if (val == 21) {analogWrite(out2, 255); delay(10); analogWrite(out2, 160) ; Serial.println(" Left Angle is = 1"); val = 100;}
-                if (val == 22) {analogWrite(out2, 255); delay(10); analogWrite(out2, 150) ; Serial.println(" Left Angle is = 2"); val = 100;}
-                if (val == 23) {analogWrite(out2, 255); delay(10); analogWrite(out2, 140); Serial.println(" Left Angle is = 3"); val = 100;}
-                if (val == 24) {analogWrite(out2, 255); delay(10); analogWrite(out2, 130); Serial.println(" Left Angle is = 4"); val = 100;}
-                if (val == 25) {analogWrite(out2, 255); delay(10); analogWrite(out2, 120); Serial.println(" Left Angle is = 5"); val = 100;}
-                if (val == 26) {analogWrite(out2, 255); delay(10); analogWrite(out2, 110); Serial.println(" Left Angle is = 6"); val = 100;}
-                if (val == 27) {analogWrite(out2, 255); delay(10); analogWrite(out2, 100); Serial.println(" Left Angle is = 7"); val = 100;}  
                             
 	}
 	if(Serial.available()) {
